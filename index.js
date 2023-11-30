07bde4ec892f8e26f62f9990158e5c73bd84eec4
Fayez Shahid <fayezshahid167@gmail.com>
2023-12-01 03:30:45 +0500

    cors added

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ connection();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 // routes
 app.use("/api/login", userRoutes);
