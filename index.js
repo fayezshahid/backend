9b96c1463109587b1b91d0165de4c16e46ba7d33
Fayez Shahid <fayezshahid167@gmail.com>
2023-12-01 03:51:04 +0500

    cors added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://rgates10412839-canary.azurewebsites.net');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
