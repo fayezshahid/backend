e97afb9f66cd49697c261ddc6441d498cb3c0824
Fayez Shahid <fayezshahid167@gmail.com>
2023-12-02 01:04:23 +0500

    random change

--- index.js
+++ index.js
@@ -13,12 +13,7 @@ connection();
 app.use(express.json());
 
 // Enable CORS for all routes
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use(cors());
 
 // routes
 app.use("/api/login", userRoutes);
