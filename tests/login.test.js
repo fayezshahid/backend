dec95c3baa5fed306b3e4372925fae32007bc568
Fayez Shahid <fayezshahid167@gmail.com>
2023-12-05 09:56:34 +0500

    edited test files

--- tests/login.test.js
+++ tests/login.test.js
@@ -6,26 +6,26 @@ chai.use(chaiHttp);
 chai.should();
 
 describe("Login Route", () => {
-  it("should log in a user with valid credentials", (done) => {
-    chai
-      .request(app)
-      .post("/api/login")
-      .send({ email: "fayezshahid167@gmail.com", password: "Fayez@2002" })
-      .end((err, res) => {
-        // Handle any potential errors
-        if (err) {
-          return done(err);
-        }
+  // it("should log in a user with valid credentials", (done) => {
+  //   chai
+  //     .request(app)
+  //     .post("/api/login")
+  //     .send({ email: "fayezshahid167@gmail.com", password: "Fayez@2002" })
+  //     .end((err, res) => {
+  //       // Handle any potential errors
+  //       if (err) {
+  //         return done(err);
+  //       }
 
-        // Make assertions on the response
-        res.should.have.status(200);
-        res.body.should.have.property("data");
-        res.body.should.have.property("message").equal("Logged in successfully");
+  //       // Make assertions on the response
+  //       res.should.have.status(200);
+  //       res.body.should.have.property("data");
+  //       res.body.should.have.property("message").equal("Logged in successfully");
 
-        // Signal that the test is complete
-        done();
-      });
-  });
+  //       // Signal that the test is complete
+  //       done();
+  //     });
+  // });
 
   it("should return 400 with invalid email and password format", (done) => {
     chai
